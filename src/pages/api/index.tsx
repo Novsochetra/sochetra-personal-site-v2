3179be4e4e943bfd08cb8d75df7258d4b55ef01e
sochetra NOV <sochetra12.nov@gmail.com>
2021-02-12 23:05:47 +0700

    style: animation slide up on blog post item

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -35,14 +35,14 @@ const sampleBlog: CardProps[] = [
     thumbnail:
       "https://cdn.pixabay.com/photo/2020/06/25/03/04/couple-5338310_1280.jpg",
   },
-  // {
-  //   title: "The WET Codebase",
-  //   description:
-  //     "In this talk, my aim was to show why strict adherence to writing code that is free of duplication inevitably leads to software we can’t understand. While you could watch this talk by yourself, I tried to make it a good starting point for a team discussion. If you drop it in Slack, tell me what your teammates thought",
-  //   publishedAt: "July 13, 2020",
-  //   thumbnail:
-  //     "https://cdn.pixabay.com/photo/2020/06/25/03/04/couple-5338310_1280.jpg",
-  // },
+  {
+    title: "The WET Codebase",
+    description:
+      "In this talk, my aim was to show why strict adherence to writing code that is free of duplication inevitably leads to software we can’t understand. While you could watch this talk by yourself, I tried to make it a good starting point for a team discussion. If you drop it in Slack, tell me what your teammates thought",
+    publishedAt: "July 13, 2020",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2020/06/25/03/04/couple-5338310_1280.jpg",
+  },
   // {
   //   title: "The WET Codebase",
   //   description:
